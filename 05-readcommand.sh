167e34c82bd9a96dc43d7fdc12026531ede2cf36
rakesh <rakesh@gmail.com>
2025-09-22 13:09:38 +0530

    Change

--- 05-readcommand.sh
+++ 05-readcommand.sh
@@ -3,6 +3,6 @@
 #usage of Read
 
 echo "Please enter the PIN number below"
-read PIN_NUMBER #here the PIN_MUNBER is variable
+read -s PIN_NUMBER #here the PIN_MUNBER is variable
 
 echo "The Pin number is : $PIN_NUMBER "
