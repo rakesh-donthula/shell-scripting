afb3bc201abd4c8f7689732d20462af231e79891
rakesh <rakesh@gmail.com>
2025-09-22 13:52:09 +0530

    Change

--- 08-Special-vars.sh
+++ 08-Special-vars.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "All the variables are : $@"
+echo "User is : $USER"
+echo "PID is : $$"
+echo "User Home directory: $HOME"
+echo "Current directory: $PWD"
+echo "Satrt date in seconds: $Date +%s "
