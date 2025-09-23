46444fbd98673ece6fd84c7321fabace143cd7c5
rakesh <rakesh@gmail.com>
2025-09-23 14:53:27 +0530

    Change

--- 13-functions.sh
+++ 13-functions.sh
@@ -16,9 +16,13 @@ VALIDATE(){
     fi
 }
 
-dnf install mysql -y
-VALIDATE $? "MYSQL"
-else "MYSQL already exists"
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else 
+    echo "MYSQL already exists"
+fi
 
 dnf install nginx -y
 VALIDATE $? "nginx"
