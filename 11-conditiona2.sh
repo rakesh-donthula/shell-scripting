6ee8703f679d212c6a01995655cf9fb148541439
rakesh <rakesh@gmail.com>
2025-09-23 14:20:56 +0530

    Change

--- 11-conditiona2.sh
+++ 11-conditiona2.sh
@@ -4,7 +4,7 @@ echo "Please enter the number"
 NUMBER=$1
 read $NUMBER
 
-if [ (($NUMBER % 2)) ]; then
+if [ (($NUMBER % 2)) -eq 0 ]; then
     echo "The given $NUMBER is EVEN"
 else
     echo "The given $NUMBER is ODD"
