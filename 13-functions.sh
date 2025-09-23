de16d150df41db0d808e5746931bbe80da4ef32d
rakesh <rakesh@gmail.com>
2025-09-23 14:43:57 +0530

    Change

---
+++ 13-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+    echo "please install the package with root"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Installation of $2 is FAILED"
+        exit 1
+    else
+        echo "Installation of $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
