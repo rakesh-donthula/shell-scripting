0e0223eaad7f2faeb366913e4a2077879c4583c8
rakesh <rakesh@gmail.com>
2025-09-23 14:29:25 +0530

    Change

--- 12-installation.sh
+++ 12-installation.sh
@@ -6,12 +6,14 @@ if [ $USER -eq 0 ]; then
     echo "Installing the package"
 else
     echo "please install the package with root"
+    exit1
 fi
 
 dnf install mysql -y
 
 if [ $? -eq 0 ]; then
     echo "Installation of MYSQL is SUCCESS"
+    exit1
 else
     echo "Installation of MYSQL is FAILED"
 fi
